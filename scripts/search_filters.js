2f18e234a44dfb067fa9b28e58106c8797f1ea57
sojoko <jonhathansojo1@gmail.com>
2023-09-12 21:51:56 -0500

    se implementa menu de opciones para filtrar las busquedas de los sitios por diferentes variables

---
+++ scripts/search_filters.js
@@ -0,0 +1,57 @@
+const sites = [
+    {
+        id: 1,
+        type: 'apartament',
+        onlyFemales: true,
+        onlyMales: false,
+        mixed: false,
+        petFriendly: false,
+        address: 'Bogota sur',
+        havePrivateBathRoom: true,
+        additionalInfo: 'ubicado cerca de la estacion de transmilenio "Portal Sur"'
+
+
+    },
+    {
+        id: 2,
+        type: 'house',
+        onlyFemales: false,
+        onlyMales: true,
+        mixed: false,
+        petFriendly: false,
+        address: 'Bogota norte',
+        havePrivateBathRoom: true,
+        additionalInfo: 'casa que en el primer piso es una muebleria y en el segundo piso es residencial, pero con acceso independiente'
+    }
+]
+
+
+console.log(sites)
+
+
+//
+// let filter = 0;
+// console.log(filter)
+// while (filter <= 0 || filter > 5 || isNaN(filter)){
+//
+//     filter = parseInt(prompt('seleccione como desea filtrar la busqueda: \n' +
+//     'PRESS 1, para buscar sitios solo para mujeres \n' +
+//     'PRESS 2, para buscar  sitios solo para hombres \n' +
+//     'PRESS 3, para buscar sitios que acepten mascotas \n' +
+//     'PRESS 4, para buscar sitios que sean apartamentos, \n' +
+//     'PRESS 5, para buscar sitios que sean casas \n') )
+//
+//     if (filter <= 0 || filter > 5 || isNaN(filter)){
+//         alert('Ha introducido un valor erroneo, por favor, intentelo nuevamente')
+//
+//     }
+//
+// }
+
+
+function typeSearh(option) {
+    if (option == 1){
+
+    }
+}
+
